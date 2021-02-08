0cb3936d806554a3d980d36adb6379816b1b01cd
Captus <40724972+LightningsFury@users.noreply.github.com>
2021-02-08 21:24:43 +0000

    server class and environment variables

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import env from "./Env";
 
-const router:express.Express = express();
+/**
+ * Server class to instantiate a http server with express routing
+ */
+class Server {
+  app: express.Express;
+  server: http.Server;
+  onStart = () => console.log("running on..." + env.hostname + env.port);
+  constructor() {
+    this.app = express();
+    this.app.use((req, res, next) => {
+      console.log(Date.now(), req.ip, req.hostname, req.body);
+      next();
+    });
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.post("/post", (req, res) => {
+      console.log(req.body);
+      res.status(200).json({ message: "pong!!!" });
+    });
+    this.server = http.createServer(this.app);
+    this.server.listen(env.port, this.onStart);
+  }
+}
 
-router.use((req, response, next) => {
-  console.log(req.ip, req.body, req.hostname);
-  next();
-});
-
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
-
-router.post("/post", (req, res) => {
-  console.log(req.body, req.body.message);
-  return res.status(200).json({ successful: true });
-});
-
-const httpServer = http.createServer(router);
-httpServer.listen(env.port, () => console.log("running on... " + env.hostname));
+new Server();
