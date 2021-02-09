3ee086b3277523296240a6f90bd9ffebdd5f4303
Captus <40724972+LightningsFury@users.noreply.github.com>
2021-02-09 23:37:33 +0000

    Json specificity and configuration changes

--- src/Env.ts
+++ src/Env.ts
@@ -5,10 +5,11 @@ dotenv.config();
 class Env {
   hostname: string;
   port: number | string;
-
+  isDev: boolean;
   constructor() {
     this.hostname = process.env.SERVER_HOSTNAME || "localhost";
     this.port = process.env.SERVER_PORT || 1337;
+    this.isDev = process.env.IS_DEV ? true : false;
   }
 }
 
