26e8e3296fa85f31d99c2881ea912e080e2bfee2
Captus <40724972+LightningsFury@users.noreply.github.com>
2021-02-10 20:31:15 +0000

    Lowkey forgot to put git in gitignore

--- src/Env.ts
+++ src/Env.ts
@@ -6,10 +6,12 @@ class Env {
   hostname: string;
   port: number | string;
   isDev: boolean;
+  password:string
   constructor() {
     this.hostname = process.env.SERVER_HOSTNAME || "localhost";
     this.port = process.env.SERVER_PORT || 1337;
     this.isDev = process.env.IS_DEV ? true : false;
+    this.password = process.env.PASSWORD || 'password123'
   }
 }
 
